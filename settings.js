f0a8fbe814ebf62367d011ba18f40c5c12ef6dcb
HackerJani00349 <110339099+HackerJani00349@users.noreply.github.com>
2024-09-02 02:34:04 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,8 +5,8 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-SESSION_ID: process.env.SESSION_ID === undefined ? '' : process.env.SESSION_ID,
-OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? '' : process.env.OWNER_NUMBER,   
+SESSION_ID: process.env.SESSION_ID === undefined ? '' : process.env.SESSION_ID,'VAJIRA-MD=KUdgHQrR#-jaeLnivG7htkTUBRU0Ljw9IGV-CrXOB9V-A8pgeFyw'
+OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? '' : process.env.OWNER_NUMBER 923069197719 923177112183,   
 ONLY_GROUP: process.env.ONLY_GROUP === undefined ? 'false' : process.env.ONLY_GROUP,
 ONLY_ME: process.env.ONLY_ME === undefined ? 'false' : process.env.ONLY_ME,
 AUTO_STATUS_READ:  process.env.AUTO_STATUS_READ  || false  ,    
